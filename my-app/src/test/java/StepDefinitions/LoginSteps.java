e23d2ed734a60ddba36bd602264a061cec89453d
fanfanafankianki <kudlatyicss@gmail.com>
2023-05-10 13:51:20 +0200

    Change in login

--- my-app/src/test/java/StepDefinitions/LoginSteps.java
+++ my-app/src/test/java/StepDefinitions/LoginSteps.java
@@ -1,6 +1,12 @@
 package StepDefinitions;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 
 import StepDefinitions.SeleniumUtility;
@@ -32,16 +38,48 @@ public class LoginSteps {
 	@When("user enters invalid (.*) and (.*)$")
 	public void user_enters_invalid_username_and_password(String username, String password) throws InterruptedException {
 		welcome = utility.getWelcomePage();
+		
 		welcome.enterLoginUsername(username);
 		welcome.enterLoginPassword(password);
 	}
 
 	@When("user enters valid (.*) and (.*)$")
 	public void user_enters_valid_username_and_password(String username, String password) throws InterruptedException {
-		welcome = utility.getWelcomePage();
-		welcome.enterLoginUsername(username);
-		welcome.enterLoginPassword(password);
+	    // Create file if not exists
+	    File counterFile = new File("src/test/java/StepDefinitions/counter.txt");
+	    if (!counterFile.exists()) {
+	        try {
+	            counterFile.createNewFile();
+	            FileWriter writer = new FileWriter(counterFile);
+	            writer.write("0");
+	            writer.close();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    // read counter from file
+	    int counter = 0;
+	    try {
+	        BufferedReader reader = new BufferedReader(new FileReader(counterFile));
+	        String line = reader.readLine();
+	        if (line != null) {
+	            counter = Integer.parseInt(line.trim());
+	        }
+	        reader.close();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+
+	    // add counter to username and password
+	    String formattedUsername = String.format("%s%d", username, counter);
+
+	    // enter valid credentials with updated username and password
+	    welcome = utility.getWelcomePage();
+	    welcome.enterLoginUsername(formattedUsername);
+	    welcome.enterLoginPassword(password);
 	}
+
 
 	@And("click on login button")
 	public void click_on_login_button() throws InterruptedException {
